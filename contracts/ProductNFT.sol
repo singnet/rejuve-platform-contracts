30d6e8873898aa6723d8c1383b3d200e6ff97709
Bushra Hussain <BushraHussain@users.noreply.github.com>
2022-05-10 17:28:09 +0500

    Inheritance Removed

--- contracts/ProductNFT.sol
+++ contracts/ProductNFT.sol
@@ -6,7 +6,7 @@ import "./Done/DataManagement.sol";
  *  that allow a registered identity to create a product.  
 */
 
-contract ProductNFT is DataManagement {
+contract ProductNFT is ERC721 {
 
     // Mapping from data hash to product UID to credit score
     mapping(bytes32 => mapping(uint => uint)) dataToProductToCredit; 
@@ -14,14 +14,28 @@ contract ProductNFT is DataManagement {
     // Mapping from Product UID to data hashes 
     mapping(uint => bytes32[]) productToData; 
 
+    IdentityToken private _identityToken;
+    DataManagement private _dataMgt; 
+
     /**
      * @dev Emitted when a new product is created 
     */
     event ProductCreated(uint creatorID, uint productUID);
 
-    constructor(string memory name_, string memory symbol_) 
-        DataManagement(name_, symbol_)
-    {}
+    constructor(string memory name_, string memory symbol_, IdentityToken identityToken_, DataManagement dataMgt_) 
+        ERC721(name_, symbol_)
+    {
+        _identityToken = identityToken_;
+        _dataMgt = dataMgt_;
+    }
+
+    /**
+     * @dev Throws if called by unregistered user.
+    */
+    modifier ifRegisteredUser {
+        require(_identityToken.ifRegistered(msg.sender) == 1, "REJUVE: Not Registered");
+        _;
+    }
     
 //------------------------------ Step 5: Creating Product - Transaction by Lab / Rejuve ---------------------
 
@@ -33,10 +47,9 @@ contract ProductNFT is DataManagement {
      * @param _dataHashes list of data hashes used in this product 
      * @param _creditScores AI assigned credit scores to each data hash
     */
-    function createProduct(uint _productCreatorId, uint _productUID, bytes32[] memory _dataHashes, uint[] memory _creditScores) external ifRegistered {
-        require(msg.sender == ownerOf(_productCreatorId), "REJUVE: Caller is not owner of lab ID");
+    function createProduct(uint _productCreatorId, uint _productUID, bytes32[] memory _dataHashes, uint[] memory _creditScores) external ifRegisteredUser  {
+        require(msg.sender == _identityToken.ownerOf(_productCreatorId), "REJUVE: Caller is not owner of lab ID");
         require(_dataHashes.length == _creditScores.length, "REJUVE: Not equal length");
-        // add into owner to token id mapping when creating shards
         _createProduct(_productUID, _dataHashes, _creditScores);
 
         emit ProductCreated(_productCreatorId, _productUID); 
@@ -48,7 +61,7 @@ contract ProductNFT is DataManagement {
      * Experimental
     */
     function linkNewData(uint _productUID, bytes32 _dataHash, uint _creditScore) external { // admin not added yet
-        require(dataToProductPermission[_dataHash][_productUID] == PermissionState.Permitted, "REJUVE: Data Not Permitted");
+        require(_dataMgt.getPermissionStatus(_dataHash, _productUID) == 1, "REJUVE: Data Not Permitted");
         productToData[_productUID].push(_dataHash);      
         dataToProductToCredit[_dataHash][_productUID] = _creditScore;  
     }
@@ -73,26 +86,28 @@ contract ProductNFT is DataManagement {
      * @notice returns owner of given data 
     */
     function getDataOwnerAddress(bytes32 _dHash) external view returns(address) {     
-        return ownerOf(dataToOwner[_dHash]);     
+        return _identityToken.ownerOf(_dataMgt.getDataOwnerId(_dHash));     
     }
 
 //-------------------------------------- Private Functions --------------------------------------------------
 
     /**
      * @notice Private function to create product NFT
-     * @dev check if all data hashes are permitted to be used in given product UID
-     * @dev Assign credit scores (by AI) to all data hashes 
-     * @dev Link product UID to all data hashes     
-     * @dev Use product UID as token id
+     * - check if all data hashes are permitted to be used in given product UID
+     * - Assign credit scores (by AI) to all data hashes 
+     * - Link product UID to all data hashes     
+     * - Use product UID as token id
     */
     function _createProduct(uint _productUID, bytes32[] memory _dataHashes, uint[] memory _creditScores) private {
 
         for(uint i = 0; i < _dataHashes.length; i++) {
-            if(dataToProductPermission[_dataHashes[i]][_productUID] == PermissionState.Permitted){ 
+
+            if(_dataMgt.getPermissionStatus(_dataHashes[i], _productUID) == 1){ 
                 dataToProductToCredit[_dataHashes[i]][_productUID] = _creditScores[i]; 
                 productToData[_productUID].push(_dataHashes[i]);
             }
             else{break;}
+
         } 
 
         _safeMint(msg.sender, _productUID); 
