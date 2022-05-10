e6ee1a33812e46ea6938b4941bdeac09f59358bd
Bushra Hussain <BushraHussain@users.noreply.github.com>
2022-05-10 18:14:08 +0500

    Contracts Moved

---
+++ contracts/DataManagement.sol
@@ -0,0 +1,169 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IdentityToken.sol";
+
+/** @dev Contract module which provides data submission, 
+ *  permission request and granting mechanism.  
+ *  It allows a caller to request specific data access.
+ *  Data owner can grant permission to access the data.
+*/
+
+contract DataManagement {
+
+    enum PermissionState { NotPermitted, Permitted, Rejected } // what's next if data owner rejects data usage request 
+
+    IdentityToken private _identityToken;
+    IERC20 private _rejuveToken; 
+
+    mapping(bytes32 => bytes32[]) dataToPermissions; // @dev test mapping if needed (during development)
+
+    // Mapping from data hash to owner identity 
+    mapping(bytes32 => uint) dataToOwner; 
+
+    // Mapping from owner identity to permission hashes
+    mapping(uint => bytes32[]) ownerToPermissions; 
+
+    // Mapping from owner identity to indexes => [dataHashes]
+    mapping(uint => uint[]) ownerToDataIndexes; 
+
+    // Mappin from OwnerIdentity to product UID to data hash
+    //mapping(uint => mapping(uint => bytes32[])) ownerToProductToData;
+
+    // Mapping from data hash to nextProductUID to permission state
+    mapping(bytes32 => mapping(uint => PermissionState)) dataToProductPermission; 
+    
+    // Array to store all data hashes 
+    bytes32[] dataHashes; 
+    
+    /**
+     * @dev Emitted when a new data hash is submitted  
+    */
+    event DataSubmitted(address dataOwner, uint dataOwnerId, bytes32 dataHash);
+
+    /**
+     * @dev Emitted when permission is requested to access specific data 
+     * to be used in a specific product
+    */
+    event PermissionRequested(uint requesterId, bytes32 dataHash, uint nextProductUID); 
+
+    /**
+     * @dev Emitted when permission is granted to access requested data 
+     * to be used in a specific product
+    */
+    event PermissionGranted(uint requesterId, bytes32 dataHash, uint nextProductUID, bytes32 permissionHash); 
+
+    constructor(IdentityToken identityToken_, IERC20 rejuveToken_) 
+    {
+        _identityToken = identityToken_;
+        _rejuveToken = rejuveToken_;
+    }
+
+    /**
+     * @dev Throws if called by any account other than data owner.
+    */
+    modifier onlyDataOwner(bytes32 _dHash) {
+        uint id = dataToOwner[_dHash];
+        require(id == _identityToken.getOwnerIdentity(msg.sender), "REJUVE: Only Data Owner");
+        _;
+    }
+
+    /**
+     * @dev Throws if called by unregistered user.
+    */
+    modifier ifRegisteredUser {
+        require(_identityToken.ifRegistered(msg.sender) == 1, "REJUVE: Not Registered");
+        _;
+    }
+
+// ---------------------------- STEP 2 : Data Submission ------------------------
+
+    /**
+     * @notice Allow only registered users to submit data. 
+     * @dev Link owner identity to submitted data hash
+    */
+    function submitData(bytes32 _dHash) external ifRegisteredUser {
+        _submitData(_dHash);
+    }
+
+//----------------- Step 3: Permission Request By researcher / Lab ---------------
+
+    /**
+     * @notice A caller can ask for permission to access specific data (Pay rejuve token for data use - add later)
+     * @dev Caller should be owner of given requester ID
+     * @param _requesterId requester identity token ID
+     * @param _nextProductUID AI suggested product unique ID 
+     * @param _dHash Data hash for which permission is requested
+    */
+
+    function requestPermission(uint _requesterId, bytes32 _dHash, uint _nextProductUID) external ifRegisteredUser { 
+        require(msg.sender == _identityToken.ownerOf(_requesterId), "REJUVE: Caller is not owner of Lab ID");
+        //check user sent amount after integrating Rejuve utility token
+
+        emit PermissionRequested(_requesterId, _dHash, _nextProductUID);
+    }
+
+//--------------------- Step 4: Grant Permission By Data Owner ---------------------
+
+    /**
+     * @notice Only data owner can grant permission 
+    */
+    function grantPermission(uint _requesterId, bytes32 _dHash, uint _nextProductUID) external ifRegisteredUser onlyDataOwner(_dHash){ // only called by owner of data
+        _grantPermission(_requesterId, _dHash, _nextProductUID);
+    }
+
+//----------------------------- OTHER SPPORTIVE VIEWS ------------------------------
+
+    /**
+     * @dev Get index of [datahashes] 
+     * Get data based on index
+    */
+    function getDataByTokenId(uint _tokenId, uint _index) external view returns(bytes32) {
+        uint index = ownerToDataIndexes[_tokenId][_index] ; // returning index of dataHashes array
+        return dataHashes[index];  
+    }
+
+    function getPermissionStatus(bytes32 _dHash, uint _productUID) external view returns(uint) {
+        return uint(dataToProductPermission[_dHash][_productUID]);
+    }
+
+    function getDataOwnerId(bytes32 _dHash) external view returns(uint) {
+        return dataToOwner[_dHash];
+    }
+
+//----------------------------- PRIVATE FUNCTIONS -----------------------------   
+
+    /**
+     * @dev Private function to submit data 
+     * - Link index of data hash to user identity
+     * - Save owner againt data 
+    */
+    function _submitData(bytes32 _dHash) private {
+        dataHashes.push(_dHash); 
+        uint index = dataHashes.length - 1;
+        uint tokenId = _identityToken.getOwnerIdentity(msg.sender); 
+        ownerToDataIndexes[tokenId].push(index); 
+        dataToOwner[_dHash] = tokenId;
+
+        emit DataSubmitted(msg.sender, tokenId, _dHash); 
+    }
+
+    /**
+     * @dev Private function to grant permission
+     * - Generate permission hash via ~keccak256
+     * - Save all permissions against each owner
+     * - Mark data permitted to be used in a specific product 
+    */    
+    function _grantPermission(uint _requesterId, bytes32 _dHash, uint _nextProductUID) private {
+        bytes32 permissionHash = _generatePermissionHash(_requesterId, _dHash, _nextProductUID);
+        ownerToPermissions[_identityToken.getOwnerIdentity(msg.sender)].push(permissionHash); // save all permissions hashes 
+        dataToProductPermission[_dHash][_nextProductUID] = PermissionState.Permitted;
+
+        emit PermissionGranted(_requesterId, _dHash, _nextProductUID, permissionHash);
+    }
+
+    function _generatePermissionHash(uint _requesterId, bytes32 _dHash, uint _nextProductUID) private pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_requesterId, _dHash, _nextProductUID));
+    }
+
+}
---
+++ contracts/IdentityToken.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/** @dev Contract module which provides an identity creation mechanism 
+ *  that allow users to create and burn (optional feature) their identities.  
+*/
+
+contract IdentityToken is ERC721 {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    enum UserStatus { NotRegistered, Registered }
+
+    // Mapping from owner to Identity token
+    mapping(address => uint) ownerToIdentity;  
+
+    // Mapping from user to registration status 
+    mapping(address => UserStatus) registrations; 
+
+    /**
+     * @dev Emitted when a new Identity is created 
+    */
+    event IdentityCreated(address caller, uint tokenId);
+
+    /**
+     * @dev Emitted when identity owner burn his token
+    */
+    event IdentityDestroyed(address owner, uint ownerId);
+
+    constructor(string memory name_, string memory symbol_)
+        ERC721(name_, symbol_)
+    {
+        _tokenIdCounter.increment();
+    }
+
+    /**
+     * @dev Throws if called by any account other than token owner.
+    */
+    modifier onlyIdentityOwner(uint _tokenId) {
+        require(_tokenId == ownerToIdentity[msg.sender], "REJUVE: Only Identity Owner");
+        _;
+    }
+
+// ---------------------------- STEP 1 : Create Identity token ------------
+  
+    /** 
+     * @notice Only one identity token per user
+     * @dev Create token and assign token id to user 
+    */
+    function createIdentity() external {
+        require(registrations[msg.sender] == UserStatus.NotRegistered, "REJUVE: One Identity Per User");
+        _createIdentity();
+    }
+      
+    /** 
+     * @notice Burn identity token 
+     * @dev only identity owner can burn his token
+    */
+    function burnIdentity(uint _tokenId) external onlyIdentityOwner(_tokenId) {
+        _burn(_tokenId);
+        registrations[msg.sender] = UserStatus.NotRegistered;
+        ownerToIdentity[msg.sender] = 0;
+
+        emit IdentityDestroyed(msg.sender, _tokenId);
+    }
+
+//----------------------------- EXTERNAL VIEWS --------------------------------
+
+    /**
+     * @dev Returns token id (Identity) of the given address.
+    */
+    function getOwnerIdentity(address _owner) external view returns(uint) { 
+        return ownerToIdentity[_owner]; 
+    }
+
+    /**
+     * @dev Returns caller registration status.
+    */
+    function ifRegistered(address _userAddress) external view returns(uint8) {
+        return uint8(registrations[_userAddress]);
+    }
+
+//----------------------------- PRIVATE FUNCTIONS -----------------------------    
+
+    /**
+     * @dev Private function to create identity token.
+     * @return uint new token id created against caller 
+    */
+    function _createIdentity() private returns(uint) { 
+        uint tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);       
+        ownerToIdentity[msg.sender] = tokenId;
+        registrations[msg.sender] = UserStatus.Registered;
+
+        emit IdentityCreated(msg.sender, tokenId);
+        return tokenId;
+    }
+
+}
