eccd728e959b81cd44dd2449e1a43a61d3762745
Bushra Hussain <BushraHussain@users.noreply.github.com>
2022-05-10 17:36:18 +0500

    Confirm RFT

---
+++ contracts/utils/ConfirmRFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../ProductNFT.sol";
+import "../Interfaces/RFT.sol";
+
+contract ConfirmRFT {
+
+    function confirmRFT(address _RFT) public view returns(bool) {
+
+        address _NFT = RFT(_RFT).parentToken(); // returns address of NFT contract
+        uint256 _tokenId = RFT(_RFT).parentTokenId(); // returns id of ID of NFT
+
+        return
+            ProductNFT(_NFT).supportsInterface(0x80ac58cd) && // confirm it is ERC-721
+            ProductNFT(_NFT).ownerOf(_tokenId) == _RFT ; // confirm the owner of the NFT is the RFT contract address
+    }
+}
