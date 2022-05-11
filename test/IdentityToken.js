aef9f9c3c4684100b7502cea675b04888941b3a9
Bushra Hussain <BushraHussain@users.noreply.github.com>
2022-05-11 23:53:17 +0500

    Test cases modified

--- test/IdentityToken.js
+++ test/IdentityToken.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 
-describe("IdentityToken contract", function () {
+describe("Identity Token Contract", function () {
 
     let _identityToken;
     let identityToken;
@@ -12,7 +12,6 @@ describe("IdentityToken contract", function () {
     const balance = 1;
 
     beforeEach(async function () {
-        // Get the ContractFactory and Signers here.
         [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
         _identityToken = await ethers.getContractFactory("IdentityToken");
         identityToken = await _identityToken.deploy("Rejuve Users","RUI");
@@ -30,34 +29,36 @@ describe("IdentityToken contract", function () {
     });
 
     it("should revert if tries to create more than one identity", async function () {
-        await identityToken.createIdentityToken();
-        await expect(identityToken.createIdentityToken())
-        .to.be.revertedWith("REJUVE: One identity per user");
+        await identityToken.createIdentity("/tokenURIHere");
+        await expect(identityToken.createIdentity("/tokenURIHere"))
+        .to.be.revertedWith("REJUVE: One Identity Per User");
     });
 
     it("should increase token id by 1 for new user", async function () {
         // user 1
-        await identityToken.createIdentityToken();
+        await identityToken.createIdentity("/tokenURIHere");
         expect (await identityToken.balanceOf(owner.address)).to.equal(balance);
-        expect (await identityToken.getOwnerId(owner.address)).to.equal(tokenId);
+        expect (await identityToken.getOwnerIdentity(owner.address)).to.equal(tokenId);
 
         // user 2
-        await identityToken.connect(addr1).createIdentityToken();
+        await identityToken.connect(addr1).createIdentity("/tokenURIHere");
         expect (await identityToken.balanceOf(addr1.address)).to.equal(balance);
-        expect (await identityToken.getOwnerId(addr1.address)).to.equal(++tokenId);
+        expect (await identityToken.getOwnerIdentity(addr1.address)).to.equal(++tokenId);
     });
 
     it("should burn given token Id", async function () {
         
-        await identityToken.createIdentityToken();
+        await identityToken.createIdentity("/tokenURIHere");
 
-        expect (await identityToken.balanceOf(owner.address)).to.equal(1);
-        expect (await identityToken.getOwnerId(owner.address)).to.equal(1);
+        expect (await identityToken.balanceOf(owner.address)).to.equal(balance);
+        expect (await identityToken.getOwnerIdentity(owner.address)).to.equal(1);
+        expect (await identityToken.ifRegistered(owner.address)).to.equal(1);
 
-        await identityToken.burnIdentity(identityToken.getOwnerId(owner.address));
+        await identityToken.burnIdentity(identityToken.getOwnerIdentity(owner.address));
         
         expect (await identityToken.balanceOf(owner.address)).to.equal(0);
-        expect (await identityToken.getOwnerId(owner.address)).to.equal(0);
+        expect (await identityToken.getOwnerIdentity(owner.address)).to.equal(0);
+        expect (await identityToken.ifRegistered(owner.address)).to.equal(0);
     });
 
 });
