45ff047bc27d7d272f4ad3fcce4ad19663ab2333
Bushra Hussain <BushraHussain@users.noreply.github.com>
2022-06-22 17:36:57 +0500

    Signature verification

--- contracts/DataManagement.sol
+++ contracts/DataManagement.sol
@@ -1,118 +1,144 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./IdentityToken.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "./Interfaces/IIdentityToken.sol";
 
-/** @dev Contract module which provides data submission, 
- *  permission request and granting mechanism.  
- *  It allows a caller to request specific data access.
- *  Data owner can grant permission to access the data.
+
+/** @dev Contract module which provides data submission and data access permission features.
+ * It allows a caller to request specific data access by taking data owner's signature as permission.
 */
 
-contract DataManagement {
+contract DataManagement is Ownable, Pausable  {
 
-    enum RequestState { NotRequested, Requested }
+    using ECDSA for bytes32;
+    IIdentityToken private _identityToken;
+
     enum PermissionState { NotPermitted, Permitted } 
 
-    IdentityToken private _identityToken;
-    IERC20 private _rejuveToken; 
-
     // Mapping from data hash to owner identity 
-    mapping(bytes32 => uint) dataToOwner; 
+    mapping(bytes => uint) private dataToOwner; 
 
     // Mapping from owner identity to permission hashes
-    mapping(uint => bytes32[]) ownerToPermissions; 
+    mapping(uint => bytes32[]) private ownerToPermissions; 
 
     // Mapping from owner identity to indexes => [dataHashes]
-    mapping(uint => uint[]) ownerToDataIndexes; 
-
-    // Mapping from data hash to nextProductUID to request state
-    mapping(bytes32 => mapping(uint => RequestState)) dataToProductRequest; 
+    mapping(uint => uint[]) private ownerToDataIndexes; 
 
     // Mapping from data hash to nextProductUID to permission state
-    mapping(bytes32 => mapping(uint => PermissionState)) dataToProductPermission; 
-    
+    mapping(bytes => mapping(uint => PermissionState)) private dataToProductPermission; 
+
+    // Mapping from data hash to nextProductUID to permission deadline
+    mapping(bytes => mapping(uint => uint)) private dataToProductToExpiry;
+
+    // Mapping from nonce to use status 
+    mapping(uint256 => bool) private usedNonces;
+
     // Array to store all data hashes 
-    bytes32[] dataHashes; 
+    bytes[] private dataHashes; 
     
     /**
      * @dev Emitted when a new data hash is submitted  
     */
-    event DataSubmitted(address dataOwner, uint dataOwnerId, bytes32 dataHash);
-
-    /**
-     * @dev Emitted when permission is requested to access specific data 
-     * to be used in a specific product
-    */
-    event PermissionRequested(uint requesterId, bytes32 dataHash, uint nextProductUID); 
-
+    event DataSubmitted(address dataOwner, uint dataOwnerId, bytes dataHash);
+ 
     /**
      * @dev Emitted when permission is granted to access requested data 
      * to be used in a specific product
     */
-    event PermissionGranted(uint requesterId, bytes32 dataHash, uint nextProductUID, bytes32 permissionHash); 
+    event PermissionGranted(uint dataOwnerId, uint requesterId, bytes dataHash, uint nextProductUID, bytes32 permissionHash); 
 
-    constructor(IdentityToken identityToken_, IERC20 rejuveToken_) 
+    constructor(IIdentityToken identityToken_) 
     {
         _identityToken = identityToken_;
-        _rejuveToken = rejuveToken_;
-    }
-
-    /**
-     * @dev Throws if called by any account other than data owner.
-    */
-    modifier onlyDataOwner(bytes32 _dHash) {
-        uint id = dataToOwner[_dHash];
-        require(id == _identityToken.getOwnerIdentity(msg.sender), "REJUVE: Only Data Owner");
-        _;
     }
 
     /**
      * @dev Throws if called by unregistered user.
     */
-    modifier ifRegisteredUser {
-        require(_identityToken.ifRegistered(msg.sender) == 1, "REJUVE: Not Registered");
+    modifier ifRegisteredUser (address _dataOwner) {
+        require(_identityToken.ifRegistered(_dataOwner) == 1, "REJUVE: Not Registered");
+        _;
+    }
+
+    /**
+     * @dev Throws if invalid signature 
+    */
+    modifier ifSignedByUserForData(address _signer, bytes memory _signature,  bytes memory _dHash, uint256 _nonce) {
+        require(_verifyDataMessage(_signature, _signer, _dHash, _nonce), "REJUVE: Invalid Signature");
         _;
     }
 
 // ---------------------------- STEP 2 : Data Submission ------------------------
 
     /**
-     * @notice Allow only registered users to submit data. 
-     * @dev Link owner identity to submitted data hash
+     * @notice Allow rejuve/sponsor to execute transaction
+     * @dev Allow only registered data owners 
+     * @dev check if data owner's signature is valid 
+     * @dev Link data owner's ID to submitted data hash
     */
-    function submitData(bytes32 _dHash) external ifRegisteredUser {
-        _submitData(_dHash);
+    function submitData(
+        address _signer, 
+        bytes memory _signature, 
+        bytes memory _dHash,
+        uint256 _nonce
+    ) 
+        external 
+        whenNotPaused
+        ifRegisteredUser(_signer)
+        ifSignedByUserForData(_signer, _signature, _dHash, _nonce) 
+    
+    {
+        _submitData(_signer,_dHash);
     }
 
-//----------------- Step 3: Permission Request By researcher / Lab ---------------
+//--------------------- Step 4: Get Permission By requester to access data ---------------------
 
     /**
-     * @notice A caller can ask for permission to access specific data (Pay rejuve token for data use - add later)
-     * @dev Caller should be owner of given requester ID
-     * @param _requesterId requester identity token ID
-     * @param _nextProductUID AI suggested product unique ID 
-     * @param _dHash Data hash for which permission is requested
+     * @notice Requester is executing transaction 
+     * @dev Requester should be a registered identity
+     * - check if requester provided correct data owner address
+     * - check if valid signature is provided
+     * - 
+     * @param _signer Data owner address 
+     * @param _signature Data owner's signature
+     * @param _dHash Data hash
+     * @param _requesterId Requester decentralized identity ID
+     * @param _nextProductUID General product ID used by requester (Lab)
+     * @param _nonce A unique number to prevent replay attacks
+     * @param _expiration A deadline 
     */
-
-    function requestPermission(uint _requesterId, bytes32 _dHash, uint _nextProductUID) external ifRegisteredUser { 
-        require(msg.sender == _identityToken.ownerOf(_requesterId), "REJUVE: Caller is not owner of ID");
-        require(dataToProductRequest[_dHash][_nextProductUID] == RequestState.NotRequested, "REJUVE: Data requested already");
-        //check user sent amount after integrating Rejuve utility token
-        dataToProductRequest[_dHash][_nextProductUID] = RequestState.Requested; 
-
-        emit PermissionRequested(_requesterId, _dHash, _nextProductUID);
+    function getPermission(
+        address _signer,
+        bytes memory _signature, 
+        bytes memory _dHash, 
+        uint256 _requesterId, 
+        uint256 _nextProductUID,
+        uint256 _nonce,
+        uint256 _expiration
+    ) 
+        external
+        whenNotPaused
+    {
+        _preValidations(msg.sender, _signer, _signature, _dHash, _requesterId, _nextProductUID, _nonce, _expiration);
+        _getPermission(_signer,_dHash ,_requesterId, _nextProductUID, _expiration);
     }
 
-//--------------------- Step 4: Grant Permission By Data Owner ---------------------
+//---------------------------- OWNER FUNCTIONS --------------------------------
+    /**
+     * @dev Triggers stopped state.
+     *
+    */
+    function pause() external onlyOwner {
+        _pause();
+    }
 
     /**
-     * @notice Only data owner can grant permission 
+     * @dev Returns to normal state.
     */
-    function grantPermission(uint _requesterId, bytes32 _dHash, uint _nextProductUID) external ifRegisteredUser onlyDataOwner(_dHash){ // only called by owner of data
-        
-        require(dataToProductRequest[_dHash][_nextProductUID] == RequestState.Requested, "REJUVE: Data not requested");
-        _grantPermission(_requesterId, _dHash, _nextProductUID);
+    function unpause() external onlyOwner {
+        _unpause();
     }
 
 //----------------------------- OTHER SPPORTIVE VIEWS ------------------------------
@@ -121,52 +147,171 @@ contract DataManagement {
      * @dev Get index of [datahashes] 
      * Get data based on index
     */
-    function getDataByTokenId(uint _tokenId, uint _index) external view returns(bytes32) {
+    function getDataByTokenId(uint _tokenId, uint _index) external view returns(bytes memory) {
         uint index = ownerToDataIndexes[_tokenId][_index] ; // returning index of dataHashes array
         return dataHashes[index];  
     }
 
-    function getPermissionStatus(bytes32 _dHash, uint _productUID) external view returns(uint8) {
+    function getPermissionStatus(bytes memory _dHash, uint _productUID) external view returns(uint8) {
         return uint8(dataToProductPermission[_dHash][_productUID]);
     }
 
-    function getDataOwnerId(bytes32 _dHash) external view returns(uint) {
+    function getDataOwnerId(bytes memory _dHash) external view returns(uint) {
         return dataToOwner[_dHash];
     }
 
-//----------------------------- PRIVATE FUNCTIONS -----------------------------   
+    function getPermissionDeadline(bytes memory _dHash, uint _nextProductUID) external view returns(uint) { 
+        return dataToProductToExpiry[_dHash][_nextProductUID];
+    }
+
+//----------------------------- PRIVATE FUNCTIONS ------------------------------------ 
 
     /**
      * @dev Private function to submit data 
      * - Link index of data hash to user identity
      * - Save owner againt data 
     */
-    function _submitData(bytes32 _dHash) private {
+    function _submitData(
+        address _dataOwner, 
+        bytes memory _dHash
+    ) 
+        private 
+    {
         dataHashes.push(_dHash); 
         uint index = dataHashes.length - 1;
-        uint tokenId = _identityToken.getOwnerIdentity(msg.sender); 
+        uint tokenId = _identityToken.getOwnerIdentity(_dataOwner); 
         ownerToDataIndexes[tokenId].push(index); 
         dataToOwner[_dHash] = tokenId;
 
-        emit DataSubmitted(msg.sender, tokenId, _dHash); 
+        emit DataSubmitted(_dataOwner, tokenId, _dHash); 
     }
 
     /**
-     * @dev Private function to grant permission
-     * - Generate permission hash via ~keccak256
-     * - Save all permissions against each owner
-     * - Mark data permitted to be used in a specific product 
-    */    
-    function _grantPermission(uint _requesterId, bytes32 _dHash, uint _nextProductUID) private {
-        bytes32 permissionHash = _generatePermissionHash(_requesterId, _dHash, _nextProductUID);
-        ownerToPermissions[_identityToken.getOwnerIdentity(msg.sender)].push(permissionHash); // save all permissions hashes 
-        dataToProductPermission[_dHash][_nextProductUID] = PermissionState.Permitted;
+     * @dev Private function to verify user signature 
+     * @dev return true if valid signature 
+    */
+    function _verifyDataMessage(
+        bytes memory _signature, 
+        address _signer, 
+        bytes memory _dHash, 
+        uint256 _nonce
+    )
+        private
+        returns (bool _flag)
+    {
+        require(!usedNonces[_nonce],"REJUVE: Signature used already");
+        usedNonces[_nonce] = true;
 
-        emit PermissionGranted(_requesterId, _dHash, _nextProductUID, permissionHash);
+        bytes32 messagehash = keccak256(abi.encodePacked(_signer, _dHash, _nonce, address(this))); // recreate message
+        address signer = messagehash.toEthSignedMessageHash().recover(_signature); // verify signer using ECDSA
+
+        if (_signer == signer) {
+            _flag = true;
+        } 
+        
     }
 
-    function _generatePermissionHash(uint _requesterId, bytes32 _dHash, uint _nextProductUID) private pure returns(bytes32) {
+    /**
+     * @dev Private function to get permission
+     * - Generate permission hash via ~keccak256
+     * - Save all permissions against each data owner ID
+     * - Mark data as "permitted" to be used in a general/next product 
+    */    
+    function _getPermission(
+        address _dataOwner,
+        bytes memory _dHash,
+        uint _requesterId,  
+        uint _nextProductUID,
+        uint _expiration 
+    ) 
+        private 
+    {
+        bytes32 permissionHash = _generatePermissionHash(_requesterId, _dHash, _nextProductUID);
+        uint dataOwnerId = _identityToken.getOwnerIdentity(_dataOwner);
+        ownerToPermissions[dataOwnerId].push(permissionHash); // save all permissions hashes 
+        dataToProductPermission[_dHash][_nextProductUID] = PermissionState.Permitted;
+        dataToProductToExpiry[_dHash][_nextProductUID] = _calculateDeadline(_expiration);
+
+        emit PermissionGranted(dataOwnerId, _requesterId, _dHash, _nextProductUID, permissionHash);
+    }
+
+    function _generatePermissionHash(
+        uint _requesterId, 
+        bytes memory _dHash, 
+        uint _nextProductUID
+    ) 
+        private 
+        pure 
+        returns(bytes32) 
+    {
         return keccak256(abi.encodePacked(_requesterId, _dHash, _nextProductUID));
+    }
+
+    /**
+     * @dev calculate permission expiration
+    */
+    function _calculateDeadline(uint _expiration) private view returns(uint) {
+        uint deadline = block.timestamp + _expiration ; 
+        return deadline;
+    }
+
+    /**
+     * @dev Pre-validations before data access permission
+     * - check if caller is registered identity
+     * - check if caller is provided correct data owner
+     * - check if valid signature is provided 
+    */
+    function _preValidations(
+        address _caller, 
+        address _signer,
+        bytes memory _signature, 
+        bytes memory _dHash, 
+        uint256 _requesterId,  
+        uint256 _nextProductUID,
+        uint256 _nonce,
+        uint256 _expiration
+    ) 
+        private 
+    {
+        require(_identityToken.ifRegistered(_caller) == 1, "REJUVE: Not Registered");
+        uint id = dataToOwner[_dHash];
+        require(id == _identityToken.getOwnerIdentity(_signer), "REJUVE: Not a Data Owner"); 
+        require(_verifyPermissionMessage(
+            _signature, 
+            _signer, 
+            _requesterId, 
+            _dHash, 
+            _nextProductUID, 
+            _nonce, 
+            _expiration
+        ), "REJUVE: Invalid Signature");       
+    }
+
+    /**
+     * @dev Private function to verify data owner's signature for permission
+     * @dev return true if valid signature 
+    */
+    function _verifyPermissionMessage(
+        bytes memory _signature, 
+        address _signer, 
+        uint256 _requesterId, 
+        bytes memory _dHash, 
+        uint256 _nextProductUID,
+        uint256 _nonce,
+        uint256 _expiration
+    )
+        private
+        returns(bool _flag)
+    {
+        require(!usedNonces[_nonce],"REJUVE: Signature used already");
+        usedNonces[_nonce] = true;
+
+        bytes32 messagehash = keccak256(abi.encodePacked(_signer, _requesterId, _dHash, _nextProductUID, _nonce, _expiration, address(this))); // recreate message
+        address signer = messagehash.toEthSignedMessageHash().recover(_signature); // verify signer using ECDSA
+
+        if (_signer == signer) {
+            _flag = true;
+        }
     }
 
 }
